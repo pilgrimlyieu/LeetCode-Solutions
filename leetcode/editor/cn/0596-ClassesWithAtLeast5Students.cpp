f180df607d2aa4b14314ccd41c8329e817aff4b9
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-09-14 12:09:21 +0800

    重命名前导零

---
+++ leetcode/editor/cn/0596-ClassesWithAtLeast5Students.cpp
@@ -0,0 +1,14 @@
+// 2025-09-14 12:06:30
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//There is no code of C++ type for this problem
+#include "../../../utils.h"
+
+int main(void) {
+    Solution s;
+    
+    return 0;
+}
