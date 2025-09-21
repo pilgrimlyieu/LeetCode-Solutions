2a339861ccf8f2f64d5a197e0926fe36ec1de03a
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-09-21 11:40:20 +0800

    W2D3(6)[9.21]: 完成 T976, 1232, 67, 50, 21, 206

---
+++ leetcode/editor/cn/0021-MergeTwoSortedLists.cpp
@@ -0,0 +1,66 @@
+// 2025-09-21 11:12:26
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        ListNode *current;
+        if (list1 == NULL && list2 == NULL) {
+            return NULL;
+        }
+        if ((list1 && list2 && list1->val < list2->val) || list2 == NULL) {
+            current = list1;
+            list1 = list1->next;
+        }
+        else {
+            current = list2;
+            list2 = list2->next;
+        }
+        ListNode *final = current;
+        while (list1 || list2) {
+            ListNode *now;
+            if ((list1 && list2 && list1->val < list2->val) || list2 == NULL) {
+                now = list1;
+                list1 = list1->next;
+            }
+            else {
+                now = list2;
+                list2 = list2->next;
+            }
+            final->next = now;
+            final = now;
+        }
+        return current;
+    }
+};
+// leetcode submit region end(Prohibit modification and deletion)
+
+#include "../../../utils.h"
+
+int main(void) {
+    Solution s;
+
+    return 0;
+}
